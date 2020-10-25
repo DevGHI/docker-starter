fce1cc19743e0ef91aa2bc373daa8c9da1389865
Si Thu Phyo <createcodes155@gmail.com>
2020-10-25 15:54:44 +0630

    Added requirements.txt

--- home/views.py
+++ home/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse("<h1>Hello World From DetailProjects</h1>")
+    return HttpResponse("<h1>Hello World From Si THu PHyo</h1>")
