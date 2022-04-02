1f6bbde2d51fa8ea479983a681e7ef1d97920e46
YeYintKo <yeyintko@ucs-myitkyina.edu.mm>
2022-04-02 14:36:27 +0630

    fixed python error

--- dockerbackend/settings.py
+++ dockerbackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#mj7vzfc5!#$$l9shqs8j+nv0@rmv@ryr!r#hpmt-m!dfek7(m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
